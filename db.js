b72c025b81295829a893adc145cde443ac5db97d
Rahul <rahul@gmail.com>
2026-02-05 23:19:04 -0800

    Fix repo: ignore node_modules, add db and init-db route

---
+++ db.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false }
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("MySQL connection failed:", err);
+  } else {
+    console.log("MySQL connected");
+  }
+});
+
+module.exports = db;
